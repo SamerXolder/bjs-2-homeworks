ae6a44a4f75511890534b6e6385807f2f617cec5
Samer <vl.sevostyano@yandex.ru>
2026-07-28 20:36:43 +0300

    Решение домашнего задание по теме Объекты добавлено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = []; 
+};
+
+const student1 = new Student ("Anna", "Famel", 19);
+const student2 = new Student ("Petr", "Male", 21);
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+  this.subject = subjectName;
+};
 
 Student.prototype.addMarks = function (...marks) {
-  
-}
+  if (Array.isArray(this.marks)) {
+    this.marks.push(...marks);
+  } 
+};
 
 Student.prototype.getAverage = function () {
+    if (Array.isArray(this.marks) && this.marks.length > 0) {
+    const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+    return sum / this.marks.length;
+  }
+  return 0;
   
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
